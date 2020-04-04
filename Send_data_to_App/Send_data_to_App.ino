4d1eb914c6ef091c14dc09e81aafe55348c06dc3
Himanshu Sharma <himanshus2847@gmail.com>
2020-04-04 17:31:21 +0530

    Add files via upload

---
+++ Send_data_to_App/Send_data_to_App.ino
@@ -0,0 +1,10 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  Serial.println("1234,5678,8765,4321,0123,"); //print the data on Serial Monitor
+  delay(1000);
+}
---
+++ Serial_test_for_bluetooth/Serial_test_for_bluetooth.ino
@@ -0,0 +1,14 @@
+void setup() 
+{
+  Serial.begin(9600);
+}
+
+void loop() 
+{
+  if(Serial.available() == 1)   //if communication is going on
+  {
+    String val = Serial.readString();   //Read the data recieved
+    Serial.print("Message from Phone: ");
+    Serial.println(val); //print the data on Serial Monitor
+  }
+}
